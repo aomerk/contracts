e005b593f65507513e1201772396076813c2a261
Ali Keser <aliomerkeser@gmail.com>
2022-03-13 15:37:41 +0300

    [test] Create test cases for ERC721 implementation

--- test/ERC721.ts
+++ test/ERC721.ts
@@ -1,16 +1,20 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+async function contracts() {
+	const ERC721 = await ethers.getContractFactory("MockERC721");
+	const erc721 = await ERC721.deploy("x");
+	await erc721.deployed();
+
+	return erc721;
+}
 describe("ERC721", function () {
 	it("deploy", async function () {
-		const ERC721 = await ethers.getContractFactory("ExampleERC721");
-		const erc721 = await ERC721.deploy();
-		await erc721.deployed();
+		contracts()
+
 	});
 	it("balanceOf", async function () {
-		const ERC721 = await ethers.getContractFactory("ExampleERC721");
-		const erc721 = await ERC721.deploy();
-		await erc721.deployed();
+		const erc721 = await contracts()
 
 		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
 
@@ -18,24 +22,55 @@ describe("ERC721", function () {
 		await erc721.balanceOf(userAddress1.address);
 		await erc721.balanceOf(userAddress2.address);
 	});
-	it("mint", async function () {
-		const ERC721 = await ethers.getContractFactory("ExampleERC721");
-		const erc721 = await ERC721.deploy();
-		await erc721.deployed();
+	it("transferFrom", async function () {
+		const erc721 = await contracts()
 
 		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
 
-		await erc721.createToken(1);
+		await erc721.mint(owner.address, 1);
+		await erc721.transferFrom(owner.address, userAddress1.address, 1);
 	});
-	it("mint a lot of stuff", async function () {
-		const ERC721 = await ethers.getContractFactory("ExampleERC721");
-		const erc721 = await ERC721.deploy();
-		await erc721.deployed();
+	it("safeTransferFrom", async function () {
+		const erc721 = await contracts()
+
+		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
+
+		await erc721.mint(owner.address, 1);
+		await erc721["safeTransferFrom(address,address,uint256)"](owner.address, userAddress1.address, 1);
+	});
+	it("approve", async function () {
+		const erc721 = await contracts()
+
+		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
+
+		await erc721.mint(owner.address, 1);
+		await erc721.transferFrom(owner.address, userAddress1.address, 1);
+		await erc721.connect(userAddress1).approve(owner.address, 1);
+	});
+	it("burn", async function () {
+		const erc721 = await contracts()
+
+		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
+
+		await erc721.mint(owner.address, 1);
+		await erc721.burn(1);
+
+	});
+	it("mint", async function () {
+		const erc721 = await contracts()
+
+		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
+
+		await erc721.mint(owner.address, 1);
+	});
+	it("mint and burn bunch of stuff", async function () {
+		const erc721 = await contracts()
 
 		const [owner, userAddress1, userAddress2] = await ethers.getSigners();
 		let items = 200;
 		for (let tokenId = 0; tokenId < items; tokenId++) {
-			await erc721.createToken(tokenId);
+			await erc721.mint(owner.address, tokenId);
+			await erc721.burn(tokenId);
 		}
 	});
 });
