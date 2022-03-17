5f3bf9c522f9947afdaa7ab996b340b2913fa27c
aomerk <aliomerkeser@gmail.com>
2022-03-17 19:49:19 +0300

    🧪 Add all combinations for ERC20 transfer from

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -9,7 +9,7 @@ async function contracts() {
 	return erc20;
 }
 const MaxUint256 = (/*#__PURE__*/ethers.BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
-describe("ERC20", function () {
+describe.only("ERC20", function () {
 	it("deploy", async function () {
 		await contracts()
 	});
@@ -36,21 +36,151 @@ describe("ERC20", function () {
 		balance = await erc20.balanceOf(owner.address)
 		expect(balance.toNumber()).to.equal(110)
 	});
-	it("transfer", async function () {
-		const erc20 = await contracts();
-		const [owner, user] = await ethers.getSigners();
-		await erc20.mint(owner.address, 120)
+	describe("transfer", async function () {
+		it("can ", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+			await erc20.mint(owner.address, 120)
 
-		let balance = await erc20.balanceOf(owner.address)
-		expect(balance.toNumber()).to.equal(120)
+			let balance = await erc20.balanceOf(owner.address)
+			expect(balance.toNumber()).to.equal(120)
 
-		await erc20.transfer(user.address, 10);
+			await erc20.transfer(user.address, 10);
 
-		balance = await erc20.balanceOf(owner.address)
-		let userBalance = await erc20.balanceOf(user.address)
+			balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
 
-		expect(balance.toNumber()).to.equal(110);
-		expect(userBalance.toNumber()).to.equal(10);
+			expect(balance.toNumber()).to.equal(110);
+			expect(userBalance.toNumber()).to.equal(10);
+		});
+		it("caller doesn't own must revert", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+			await erc20.mint(owner.address, 120)
+
+			let balance = await erc20.balanceOf(owner.address)
+			expect(balance.toNumber()).to.equal(120)
+
+			await expect(
+				erc20.connect(user).transfer(user.address, 10)
+			).to.be.reverted;
+			balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(120);
+			expect(userBalance.toNumber()).to.equal(0);
+		});
+	});
+	describe("transferFrom", async function () {
+		it("reverts on unauth ", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+
+			await expect(erc20.connect(user).transferFrom(owner.address, user.address, 10)).to.be.reverted;
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(120);
+			expect(userBalance.toNumber()).to.equal(0);
+		});
+		it("allows transfer on behalf of ", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+			await erc20.approve(user.address, 10);
+
+			await erc20.connect(user).transferFrom(owner.address, user.address, 10)
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(110);
+			expect(userBalance.toNumber()).to.equal(10);
+		});
+		it("from == sender, not approved must revert", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+
+			await erc20.approve(user.address, 10);
+
+			await expect(erc20.connect(owner).
+				transferFrom(owner.address, user.address, 10)).to.be.reverted;
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(120);
+			expect(userBalance.toNumber()).to.equal(0);
+		});
+		it("from == sender,	approved", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+			await erc20.approve(owner.address, 10);
+
+			await erc20.connect(owner).transferFrom(owner.address, user.address, 10)
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(110);
+			expect(userBalance.toNumber()).to.equal(10);
+		});
+		it("from != sender,	not approved must revert", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+
+			await expect(
+				erc20.connect(user).transferFrom(owner.address, user.address, 10)
+			).to.be.reverted;
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(120);
+			expect(userBalance.toNumber()).to.equal(0);
+		});
+		it("from != sender,	approved", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+			await erc20.approve(user.address, 10);
+
+			await erc20.connect(user).transferFrom(owner.address, user.address, 10)
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(110);
+			expect(userBalance.toNumber()).to.equal(10);
+		});
+		it("from != sender,	approved but spends too much must revert", async function () {
+			const erc20 = await contracts();
+			const [owner, user] = await ethers.getSigners();
+
+			await erc20.mint(owner.address, 120)
+			await erc20.approve(user.address, 10);
+
+			await expect(
+				erc20.connect(user).transferFrom(owner.address, user.address, 20)
+			).to.be.reverted;
+
+			let balance = await erc20.balanceOf(owner.address)
+			let userBalance = await erc20.balanceOf(user.address)
+
+			expect(balance.toNumber()).to.equal(120);
+			expect(userBalance.toNumber()).to.equal(0);
+		});
 	});
 	it("approve", async function () {
 		const erc20 = await contracts();
