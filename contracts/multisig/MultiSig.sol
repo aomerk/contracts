37aeb0f8462b6db4d98544dc57de3e2106a81815
aomerk <aliomerkeser@gmail.com>
2022-03-23 12:10:22 +0100

    🚧 Define event and structs

--- contracts/multisig/MultiSig.sol
+++ contracts/multisig/MultiSig.sol
@@ -1,4 +1,41 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.0;
 
-contract MultiSig {}
+contract MultiSig {
+    /************************************
+     *									*
+     *				events				*
+     *									*
+     ************************************/
+    event Submit(
+        address indexed _owner,
+        address indexed _to,
+        uint256 indexed _transactionId,
+        uint256 _value,
+        bytes _data
+    );
+
+    event Confirm(
+        address indexed _owner,
+        address indexed _to,
+        uint256 indexed _transactionId,
+        uint256 _value,
+        bytes _data
+    );
+
+    event Deposit(address indexed _owner, uint256 _value);
+    event Execution(uint256 indexed _transactionId, bytes data);
+
+    /************************************
+     *									*
+     *			definitions 			*
+     *									*
+     ************************************/
+    struct Transaction {
+        address _to;
+        uint256 _value;
+        bool _executed;
+        bytes data;
+    }
+
+}
